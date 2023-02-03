1d585fbb6c2f460785d514dd1f7f652e12b2bb70
Franek Antoniak <franekant123@gmail.com>
2023-02-03 02:46:51 +0100

    other http methods and tests for beer api in brewery client

--- mssc-beer-client/src/main/java/springframework/msscbeerclient/client/BreweryClient.java
+++ mssc-beer-client/src/main/java/springframework/msscbeerclient/client/BreweryClient.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 import springframework.msscbeerclient.model.BeerDto;
 
+import java.net.URI;
 import java.util.UUID;
 
 @Component
@@ -16,5 +17,17 @@ public class BreweryClient {
 
 	public BeerDto getBeerById(UUID uuid) {
 		return restTemplate.getForObject(apihost + BEER_PATH_V1 + uuid.toString(), BeerDto.class);
+	}
+
+	public URI saveNewBeer(BeerDto beerDto) {
+		return restTemplate.postForLocation(apihost + BEER_PATH_V1, beerDto);
+	}
+
+	public void updateBeer(UUID uuid, BeerDto beerDto) {
+		restTemplate.put(apihost + BEER_PATH_V1 + "/" + uuid.toString(), beerDto);
+	}
+
+	public void deleteBeer(UUID uuid) {
+		restTemplate.delete(apihost + BEER_PATH_V1 + "/" + uuid.toString());
 	}
 }
--- mssc-beer-client/src/test/java/springframework/msscbeerclient/client/BreweryClientTest.java
+++ mssc-beer-client/src/test/java/springframework/msscbeerclient/client/BreweryClientTest.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import springframework.msscbeerclient.model.BeerDto;
 
+import java.net.URI;
 import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -19,5 +20,28 @@ class BreweryClientTest {
 		BeerDto beerDto = breweryClient.getBeerById(UUID.randomUUID());
 
 		assertNotNull(beerDto);
+	}
+
+	@Test
+	void saveNewBeer() {
+		//given
+		BeerDto beerDto = BeerDto.builder().beerName("New Beer").build();
+
+		URI uri = breweryClient.saveNewBeer(beerDto);
+
+		assertNotNull(uri);
+	}
+
+	@Test
+	void updateBeer() {
+		//given
+		BeerDto beerDto = BeerDto.builder().beerName("New Beer").build();
+
+		breweryClient.updateBeer(UUID.randomUUID(), beerDto);
+	}
+
+	@Test
+	void deleteBeer() {
+		breweryClient.deleteBeer(UUID.randomUUID());
 	}
 }
