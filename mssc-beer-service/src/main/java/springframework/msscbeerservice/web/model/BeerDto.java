551027c1c65171d0a7f84d261a7b10f7fb6c3e95
Franek Antoniak <franekant123@gmail.com>
2023-02-03 06:33:38 +0100

    BeerDto validation in mssc-beer-service service

--- mssc-beer-service/src/main/java/springframework/msscbeerservice/web/model/BeerDto.java
+++ mssc-beer-service/src/main/java/springframework/msscbeerservice/web/model/BeerDto.java
@@ -5,6 +5,7 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.*;
 import java.math.BigDecimal;
 import java.time.OffsetDateTime;
 import java.util.Currency;
@@ -15,19 +16,36 @@ import java.util.UUID;
 @NoArgsConstructor
 @AllArgsConstructor
 public class BeerDto {
+	@Null
 	private UUID id;
+
+	@Null
 	private Integer version;
 
+	@Null
 	private OffsetDateTime createdDate;
+
+	@Null
 	private OffsetDateTime lastModifiedDate;
 
+	@NotBlank
 	private String beerName;
 
+	@NotNull
 	private BeerStyleEnum beerStyle;
 
+	@Positive
+	@NotNull
 	private Long upc;
 
+	@Positive
+	@NotNull
 	private BigDecimal price;
+	@NotNull
+	@org.hibernate.validator.constraints.Currency({"USD"})
 	private Currency currency;
+
+	@NotNull
+	@PositiveOrZero
 	private Integer quantityOnHand;
 }
