73dc2a39e76a2b565124eeeca8be860b6d9f2d9a
Franek Antoniak <franekant123@gmail.com>
2023-02-03 01:06:49 +0100

    adding web data model

---
+++ mssc-beer-service/src/main/java/springframework/msscbeerservice/web/model/BeerDto.java
@@ -0,0 +1,33 @@
+package springframework.msscbeerservice.web.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.OffsetDateTime;
+import java.util.Currency;
+import java.util.UUID;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class BeerDto {
+	private UUID id;
+	private Integer version;
+
+	private OffsetDateTime createdDate;
+	private OffsetDateTime lastModifiedDate;
+
+	private String beerName;
+
+	private BeerStyleEnum beerStyle;
+
+	private Long upc;
+
+	private BigDecimal price;
+	private Currency currency;
+	private Integer quantityOnHand;
+}
