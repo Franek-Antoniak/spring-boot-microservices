60091a36216a7d81d402994bbe3c43f5bd5ab3ea
Franek Antoniak <franekant123@gmail.com>
2023-02-03 01:48:52 +0100

    mssc-beer-client initial components

---
+++ mssc-beer-client/src/main/java/springframework/msscbeerclient/MsscBeerClientApplication.java
@@ -0,0 +1,13 @@
+package springframework.msscbeerclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsscBeerClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsscBeerClientApplication.class, args);
+	}
+
+}
---
+++ mssc-beer-client/src/test/java/springframework/msscbeerclient/MsscBeerClientApplicationTests.java
@@ -0,0 +1,13 @@
+package springframework.msscbeerclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsscBeerClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
