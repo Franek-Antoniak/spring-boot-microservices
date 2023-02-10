27a15b8fc36d02a1695b2158baccb0a7dce0e382
Franek Antoniak <franekant123@gmail.com>
2023-02-10 16:52:48 +0100

    initial configuration for state machine

---
+++ spring-state-machine/src/test/java/springframework/spring/state/machine/SpringStateMachineApplicationTests.java
@@ -0,0 +1,13 @@
+package springframework.spring.state.machine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringStateMachineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
