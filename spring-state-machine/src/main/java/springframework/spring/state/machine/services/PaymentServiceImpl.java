29e5a9f9c2301b95abfbf92447263aeaed4e3858
Franek Antoniak <franekant123@gmail.com>
2023-02-10 17:36:32 +0100

    getting statemachine for payment

--- spring-state-machine/src/main/java/springframework/spring/state/machine/services/PaymentServiceImpl.java
+++ spring-state-machine/src/main/java/springframework/spring/state/machine/services/PaymentServiceImpl.java
@@ -1,28 +1,54 @@
 package springframework.spring.state.machine.services;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.statemachine.StateMachine;
+import org.springframework.statemachine.config.StateMachineFactory;
+import org.springframework.statemachine.support.DefaultStateMachineContext;
 import springframework.spring.state.machine.domain.Payment;
 import springframework.spring.state.machine.domain.PaymentEvent;
 import springframework.spring.state.machine.domain.PaymentState;
+import springframework.spring.state.machine.repository.PaymentRepository;
 
+@RequiredArgsConstructor
 public class PaymentServiceImpl implements PaymentService {
+	private final PaymentRepository paymentRepository;
+	private final StateMachineFactory<PaymentState, PaymentEvent> stateMachineFactory;
+
 	@Override
 	public Payment newPayment(Payment payment) {
-		return null;
+		payment.setState(PaymentState.NEW);
+		return paymentRepository.save(payment);
 	}
 
 	@Override
 	public StateMachine<PaymentState, PaymentEvent> preAuth(Long paymentId) {
-		return null;
+		StateMachine<PaymentState, PaymentEvent> sm = build(paymentId);
+		return sm;
 	}
 
 	@Override
 	public StateMachine<PaymentState, PaymentEvent> authorizePayment(Long paymentId) {
-		return null;
+		StateMachine<PaymentState, PaymentEvent> sm = build(paymentId);
+		return sm;
 	}
 
 	@Override
 	public StateMachine<PaymentState, PaymentEvent> declineAuth(Long paymentId) {
-		return null;
+		StateMachine<PaymentState, PaymentEvent> sm = build(paymentId);
+		return sm;
+	}
+
+	private StateMachine<PaymentState, PaymentEvent> build(Long paymentId) {
+		Payment payment = paymentRepository.findById(paymentId)
+		                                   .orElseThrow();
+		StateMachine<PaymentState, PaymentEvent> sm = stateMachineFactory.getStateMachine(
+				Long.toString(payment.getId()));
+		sm.stop();
+		sm.getStateMachineAccessor()
+		  .doWithAllRegions(sma -> {
+			  sma.resetStateMachine(new DefaultStateMachineContext<>(payment.getState(), null, null, null));
+		  });
+		sm.start();
+		return sm;
 	}
 }
