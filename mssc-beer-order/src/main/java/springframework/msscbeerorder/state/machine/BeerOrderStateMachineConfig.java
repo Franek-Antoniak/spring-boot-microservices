2fe776d957f8d92166438664e8432f9b44cd584a
Franek Antoniak <franekant123@gmail.com>
2023-02-11 10:53:54 +0100

    refactor order states and events

--- mssc-beer-order/src/main/java/springframework/msscbeerorder/state/machine/BeerOrderStateMachineConfig.java
+++ mssc-beer-order/src/main/java/springframework/msscbeerorder/state/machine/BeerOrderStateMachineConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.statemachine.config.EnableStateMachineFactory;
 import org.springframework.statemachine.config.StateMachineConfigurerAdapter;
 import org.springframework.statemachine.config.builders.StateMachineStateConfigurer;
+import org.springframework.statemachine.config.builders.StateMachineTransitionConfigurer;
 import springframework.msscbeerorder.domain.BeerOrderEventEnum;
 import springframework.msscbeerorder.domain.BeerOrderStatusEnum;
 
@@ -26,5 +27,24 @@ public class BeerOrderStateMachineConfig extends StateMachineConfigurerAdapter<B
 		      .end(BeerOrderStatusEnum.PICKED_UP);
 	}
 
+	@Override
+	public void configure(
+			StateMachineTransitionConfigurer<BeerOrderStatusEnum, BeerOrderEventEnum> transitions) throws Exception {
+		transitions.withExternal()
+		           .source(BeerOrderStatusEnum.NEW)
+		           .target(BeerOrderStatusEnum.VALIDATION_PENDING)
+		           .event(BeerOrderEventEnum.VALIDATE_ORDER)
+		           .and()
+		           .withExternal()
+		           .source(BeerOrderStatusEnum.NEW)
+		           .target(BeerOrderStatusEnum.VALIDATED)
+		           .event(BeerOrderEventEnum.VALIDATION_PASSED)
+		           .and()
+		           .withExternal()
+		           .source(BeerOrderStatusEnum.NEW)
+		           .target(BeerOrderStatusEnum.VALIDATION_EXCEPTION)
+		           .event(BeerOrderEventEnum.VALIDATION_FAILED)
+		           .and();
+	}
 
 }
